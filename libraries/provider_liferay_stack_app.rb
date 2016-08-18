df5d5601236b80e850688fcbd8423fa0cb614fe2
Adam Krone <krone.adam@gmail.com>
2016-08-18 16:01:02 -0500

    Adds 'provides' information for LWRPs

--- libraries/provider_liferay_stack_app.rb
+++ libraries/provider_liferay_stack_app.rb
@@ -25,6 +25,7 @@ class Chef
     class LiferayStackApp < Chef::Provider::LWRPBase
       include Chef::DSL::IncludeRecipe
       use_inline_resources if defined?(use_inline_resources)
+      provides :liferay_stack_app
 
       def whyrun_supported?
         true
